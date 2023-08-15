9366b1e809fd77608e1783f4657042677b662eb6
Guilherme Silva <guilherme.silva@empow.com.br>
2023-08-15 10:15:30 -0300

    Feat: add Watson api and get keywords of all sentences

--- robots/text.ts
+++ robots/text.ts
@@ -1,16 +1,26 @@
 import { Content } from "../models/content";
 import wiki from 'wikijs';
+
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
 const sentenceBoundaryDetection = require('sbd');
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey;
+const nlu = new NaturalLanguageUnderstandingV1({
+  authenticator: new IamAuthenticator({ apikey: watsonApiKey }),
+  version: '2018-04-05',
+  serviceUrl: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com'
+});
+
 
 export async function textRobot(content: Content) {
     await fetchContentFromWikipedia(content);
     breakContentIntoSentences(content);
     limitMaximumSentences(content);
-
+    await fetchKeywordsOfAllSentences(content);
 
     async function fetchContentFromWikipedia(content: Content) {
         const wikipediaResponse = (await wiki().page(content.searchTerm).then(page => page.summary())).toString();
-        const withoutDatesInParentheses = removeDatesInParentheses(wikipediaResponse)
+        const withoutDatesInParentheses = removeDatesInParentheses(wikipediaResponse);
 
         content.sourceContentOriginal = wikipediaResponse;
         content.sourceContentSanitized = withoutDatesInParentheses;
@@ -34,5 +44,29 @@ export async function textRobot(content: Content) {
 
     function limitMaximumSentences(content: Content) {
         content.sentences = content.sentences.slice(0, content.maximumSentences);
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            })
+            .then((response: any) => {
+                const keywords = response.result.keywords.map((keyword: any) => {
+                    return keyword.text;
+                })
+
+                resolve(keywords);
+            })
+        })
+    }
+
+    async function fetchKeywordsOfAllSentences(content: Content) {
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text);
+        }
     }
 }
