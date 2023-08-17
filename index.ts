d40445fba3f94bfb4a62c1652b2a55d3095de02c
Guilherme Silva <guilherme.silva@empow.com.br>
2023-08-17 17:33:10 -0300

    Feat: cleaning some code

--- index.ts
+++ index.ts
@@ -1,19 +1,13 @@
 import { textRobot } from "./robots/text";
 import { inputRobot } from "./robots/input";
-import { load } from "./robots/state";
 import { imageRobot } from "./robots/image";
 import { videoRobot } from "./robots/video";
 
 async function start() {
-    // inputRobot();
-    // await textRobot();
-    // await imageRobot();
+    inputRobot();
+    await textRobot();
+    await imageRobot();
     await videoRobot();
-
-    const content = load();
-
-    // console.dir(content, { depth: null });
-    // console.log('> Done!');
 }
 
 start();
