974394256ec751d109d08c991f69915d5c4f4f78
Guilherme Silva <guilherme.silva@empow.com.br>
2023-08-17 17:30:20 -0300

    Feat: add FULL video robot

--- index.ts
+++ index.ts
@@ -2,17 +2,18 @@ import { textRobot } from "./robots/text";
 import { inputRobot } from "./robots/input";
 import { load } from "./robots/state";
 import { imageRobot } from "./robots/image";
+import { videoRobot } from "./robots/video";
 
 async function start() {
-    inputRobot();
-    await textRobot();
-    await imageRobot();
+    // inputRobot();
+    // await textRobot();
+    // await imageRobot();
+    await videoRobot();
 
     const content = load();
 
     // console.dir(content, { depth: null });
-    console.log('> Done!');    
-
+    // console.log('> Done!');
 }
 
 start();
